dcb34ad3628bd03ceb2e46295b8fe4a50bfeab5b
kato <user@MacBook-Air.local>
2020-07-14 01:50:53 +0900

    Update unicorn config to listen on NGINX socket.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,5 @@
 require 'fileutils'
-
-worker_processes 4
-listen 'unix:///tmp/nginx.socket', backlog: 1024
+listen '/tmp/nginx.socket'
 before_fork do |server,worker|
 	FileUtils.touch('/tmp/app-initialized')
 end
